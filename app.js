431fb5a98b1db3e02d05de306643ee02292a8875
Tanshi Khandelwal <tanshikhandelwal03@gmail.com>
2025-07-21 18:53:49 +0530

    Fixed!!

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT           // ✅ ADD THIS LINE
 });
-
 
 // Connect to MySQL
 db.connect((err) => {
